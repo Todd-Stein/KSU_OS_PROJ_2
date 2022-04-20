a11f2926a3500b7615ed44d28ef7e3f433b62b88
Todd-Stein <tpstein19@gmail.com>
2022-04-20 11:33:08 -0400

    renamed a function and added some comments

--- bankers.c
+++ bankers.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
-int inOrder(int a[], int b[], int n) {
+//this function checks if 2 1-D arrays are equal, takes 2 arrays and size, I made this since checking for equivelence directly for arrays with == operator is syntactically valid but doesn't work
+int arrsAreEqual(int a[], int b[], int n) {
 	for(int i = 0; i<n; n++) {
 		if(a[i] != b[i])
 			return 0;
@@ -24,15 +25,18 @@ int main() {
 				 {2, 1, 1},
 				 {0, 0, 2}};
 	int need[5] [3];
+	//fill out need matrix
 	for(int i = 0; i<numProcesses; i++) {
 		for(int j = 0; j<numResources; j++) {
 			need[i][j] = fabs(max[i][j] - allocation[i][j]);
 		}
 	}
+	//keeps track of which processes have been executed
 	int processCanExecute[5] = {0, 0, 0, 0, 0};
+	//for testing to see if each process has been executed
 	int processesHaveExecuted[5] = {1, 1, 1, 1, 1};
 	printf("Safe sequence:\n");
-	while(inOrder(processCanExecute, processesHaveExecuted, numProcesses) != 1) {
+	while(arrsAreEqual(processCanExecute, processesHaveExecuted, numProcesses) != 1) {
 		for(int i = 0; i<numProcesses; i++) {
 			for(int j = 0; j<numResources; j++) {
 				if(processCanExecute[i] == 0) {
@@ -45,10 +49,10 @@ int main() {
 					}
 				}
 			}
+			//this is so the process prints it's name only once
 			if(processCanExecute[i] == 2) {
 				for(int j = 0; j<numResources; j++) {
 					available[j] = available[j] + need[i][j];
-					//printf("%d\n",available[j]);
 				}
 				printf("Process %d\n", i);
 				processCanExecute[i] = 1;
